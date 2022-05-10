8d3a524db430981a23c30796e4e252acbebce1c6
jueon <wndjs803@pusan.ac.kr>
2022-05-10 21:27:42 +0900

    Add a mul function

--- calc.c
+++ calc.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int add(int a, int b);
+int mul(int a, int b);
 
 int main() {
 	int a, b;
 	scanf("%d %d", &a, &b);
 	printf("%d + %d = %d\n", a, b, add(a, b));
+	printf("%d * %d = %d\n", a, b, mul(a, b));
 }
 
